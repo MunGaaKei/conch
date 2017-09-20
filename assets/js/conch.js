5452800d6919bb09f4b3f65fc493335a040d62f4
Gaakei <502975759@qq.com>
2017-09-20 17:33:26 +0800

    build: 9-20

--- assets/js/conch.js
+++ assets/js/conch.js
@@ -11,7 +11,8 @@
 
         eventListen: () => {
 
-            let main = doc.querySelector('div.main');
+            let main = doc.querySelector('div.main'),
+                mainLeft = 0;
 
             let widen = (e) => {
                 let left = e.clientX < 76.8? 76.8: e.clientX;
@@ -24,13 +25,24 @@
             doc.querySelectorAll('.winctl i')[1].onclick = () => { ipc.send('maximize-app'); }
             doc.querySelectorAll('.winctl i')[0].onclick = () => { ipc.send('minimize-app'); }
 
+            // 侧边栏显示与隐藏
+            doc.querySelector('.menuctl').onclick = () => {
+                if(main.getBoundingClientRect().left === 0){
+                    main.style.left = mainLeft + 'px';
+                } else {
+                    mainLeft = main.getBoundingClientRect().left;
+                    main.style.left = 0;
+                }
+            }
 
             // 鼠标监听改变内容区宽度
             doc.querySelector('span.widen').addEventListener('mousedown', () => {
+                main.style.transition = 'none';
                 doc.addEventListener('mousemove', widen);
                 doc.addEventListener('mouseup', () => {
                     doc.removeEventListener('mousemove', widen);
                     this.removeEventListener('mouseup', arguments.callee);
+                    main.style.transition = 'all .2s ease';
                 });
             });
 
