b3307349777d17ef1222495de96caaac244573d9
Ian Man <ian@mmldigi.com>
2019-12-24 18:01:34 +0800

    conch build

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,7 +1,24 @@
 ;(function(win, doc){
 
     const ipc = require('electron').ipcRenderer;
+    const $header = doc.querySelector('.header');
 
+    let directHeader = act => {
+        switch( act ){
+            case 'min': ipc.send('app-minimize'); break;
+            case 'close': ipc.send('app-close'); break;
+            default: break;
+        }
+    }
 
+    $header.addEventListener('click', e => {
+        let act,
+            tar = e.target;
+        while( tar !== $header ){
+            act = tar.dataset.act;
+            act && directHeader( act );
+            tar = tar.parentNode;
+        }
+    });
 
 })(window, document);
