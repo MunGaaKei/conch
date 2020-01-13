841cc271c9e3a28260d0b81c9026e36fc9b468b4
Ian Man <ian@mmldigi.com>
2020-01-13 18:01:01 +0800

    conch build

--- assets/js/core.js
+++ assets/js/core.js
@@ -5,28 +5,65 @@
     const RL = require('readline');
 
     const $header = doc.querySelector('.header');
+    const $tabs = doc.querySelector('.tabs');
     const $content = doc.querySelector('.content');
 
-    let WORKSPACE = [];
+    let WORKSPACE = win.localStorage.tabs;
+    WORKSPACE = WORKSPACE? JSON.parse(WORKSPACE): {
+        0: {
+            path: undefined,
+            title: '未命名',
+            saved: true
+        },
+        // 1: {
+        //     path: 'D:/1AN/conch/documents/demo.txt',
+        //     title: '未命名',
+        //     saved: true
+        // },
+        // 2: {
+        //     path: 'D:/1AN/conch/documents/demo.docs',
+        //     title: '未命名的文档',
+        //     saved: true
+        // }
+    };
+
+
+    // 监听 WORKSPACE 变化改变 tabs 状态
+    let monitor = files => {
+        let handler = new Proxy( files, {
+            set: ( target, prop, val ) => {
+                console.log( target, prop, val );
+                target[prop] = val;
+                return true;
+            }
+        });
+
+        handler[2] = 123;
+    }
 
     // 指令
-    let directive = ( act = '', context ) => {
+    let directive = ( act = '', tar ) => {
         switch( act ){
             case 'min': IPC.send('app-minimize'); break;
-            case 'close': IPC.send('app-close'); break;
-            case 'save': break;
+            case 'exit':
+                // 检查是否还有文档未保存
+                IPC.send('app-close');
+                break;
             default: break;
         }
     }
 
     // 打开文件
-    let open = (id, path) => {
-
+    let open = id => {
+        let file = WORKSPACE[id];
+        file && readFile( file.path ).then( res => {
+            doc.querySelector('.editor').innerHTML = res;
+        });
     }
 
     // 读取文件
-    let readTXT = async files => {
-        const stream = FS.createReadStream( files );
+    let readFile = async file => {
+        const stream = FS.createReadStream( file );
         const lines = RL.createInterface({
             input: stream,
             crlfDelay: Infinity,
@@ -42,37 +79,63 @@
         return content;
     }
 
+    // 保存文件
+    let save = ( id, content ) => {
+
+    }
+
     $header.addEventListener('click', e => {
+        e.stopPropagation();
         let act,
             tar = e.target;
         while( tar !== $header ){
             act = tar.dataset.act;
-            act && directive( act, $header );
+            act && directive( act, tar );
             tar = tar.parentNode;
         }
     });
 
-    doc.addEventListener('dragover', e => { e.preventDefault(); });
-    doc.addEventListener('drop', e => {
+    $content.addEventListener('dragover', e => { e.preventDefault(); });
+    $content.addEventListener('drop', e => {
         e.preventDefault();
-        console.log(e.dataTransfer.files[0]);
-        
-        readTXT( e.dataTransfer.files[0].path ).then( res => {
 
-            console.log(res);
+        let file = e.dataTransfer.files[0];
+        file && readFile( file.path ).then( res => {
+
+            // doc.querySelector('.editor').innerHTML = res;
 
         });
     });
     
     // 监听键盘输入
-    doc.addEventListener('keydown', e => {
+    $content.addEventListener('keydown', e => {
         switch( e.keyCode ){
-            case 9: // tab
+            case 9:     // tab
                 e.preventDefault();
                 doc.execCommand('insertHTML', doc.getSelection(), ' '.repeat(4));
                 break;
             default: break;
         }
     });
+
+    $content.addEventListener('keyup', e => {
+        console.log(e);
+        
+        switch( e.keyCode ){
+            case 83:    // save: ctrl+s
+                if( e.ctrlKey ){
+                    
+                }
+                break;
+            default: break;
+        }
+    });
+
+    
+
+
+    monitor( WORKSPACE );
+
+
 
 })(window, document);
